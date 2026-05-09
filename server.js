f1e6aa6c50cf8d9044f9043f14aa93b0a6bc7273
Simon Gatuku <gatukusimon6@gmail.com>
2026-05-09 16:26:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/posts", postRoutes);
+
+app.get("/", 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
